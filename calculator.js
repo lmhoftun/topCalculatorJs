824eae00d090833287023b57031ab30e297a74f9
lmhoftun <54537267+lmhoftun@users.noreply.github.com>
2020-01-16 10:25:51 +0100

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,18 +1,11 @@
-let textToDisplay = '0';
-function disp(btn){
-    
-    let key = document.getElementById(btn).innerHTML;
-    let s = textToDisplay.concat(key);
 
-    document.getElementById("display").value=s;
-    console.log(key);
-    console.log(s);
-}
 const buttons = document.querySelectorAll('button');
 
 buttons.forEach((button) => {
     button.addEventListener('click', (e) => {
-        disp(button.id);
+        let key = document.getElementById(button.id).innerHTML;
+        console.log(key);
+        
 
         
         
