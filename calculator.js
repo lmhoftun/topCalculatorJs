83f992a186a94011ef294ab28c51f7bad9b977f9
lmhoftun <lmlindbo@gmail.com>
2020-01-20 00:06:32 +0100

    seperating numbers and operands

--- calculator.js
+++ calculator.js
@@ -1,21 +1,40 @@
-let textToDisplay = '0';
-function disp(btn){
+/*function showDisplay(key){
+    let disp = document.getElementById('display').value;
     
-    let key = document.getElementById(btn).innerHTML;
-    let s = textToDisplay.concat(key);
+    document.getElementById('display').value += key;
 
-    document.getElementById("display").value=s;
-    console.log(key);
-    console.log(s);
+}*/
+let disp = document.getElementById('display').value;
+let currentOperand = '';
+
+//try to check if number or operand
+function checkNumber(key){
+    if(!isNaN(key)){
+        
+        disp += key;
+        document.getElementById('display').value = disp;
+    } else {
+        currentOperand = key;
+        console.log(currentOperand);
+
+
+    }
+    console.log("checknumber " + key + "Display "+ disp);
+
 }
 const buttons = document.querySelectorAll('button');
-
 buttons.forEach((button) => {
     button.addEventListener('click', (e) => {
-        disp(button.id);
+        event.preventDefault()
+        let key = document.getElementById(button.id).innerHTML;
+        console.log(key);
+        checkNumber(key);
+//        showDisplay(key);
 
         
         
     });
     
 });
+
+//showDisplay(0);
